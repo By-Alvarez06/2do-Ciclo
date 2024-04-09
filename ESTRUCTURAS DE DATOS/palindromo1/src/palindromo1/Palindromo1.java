a844c681df3ccce03bebad7baa82fc0696b9631c
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-04-09 17:09:58 -0500

    Ejercicio 2

--- ESTRUCTURAS DE DATOS/palindromo1/src/palindromo1/Palindromo1.java
+++ ESTRUCTURAS DE DATOS/palindromo1/src/palindromo1/Palindromo1.java
@@ -1,10 +1,15 @@
 package palindromo1;
+import java.util.Scanner;
 public class Palindromo1 {
     public static void main(String[] args) {
-        String str1 = "Se verlas al reves";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese una frase: ");
+        String str1 = sc.nextLine();
         System.out.println("Frase: " + str1); //Muestra la frase en pantalla
         str1 = str1.toLowerCase(); //Hace toda la frase en minusculas
         str1 = str1.replaceAll(" +", "");//Elimina todos los espacios
+        str1 = str1.replaceAll(",", "");//Elimina todos las comas
+        str1 = str1.replaceAll(".", "");//Elimina todos los puntos
         boolean palind = true;//Booleano para determinar si es o no Palindromo
         for (int i = 0; i < (str1.length()/2); i++){
             if (str1.charAt(i) != str1.charAt(str1.length()-1-i)){
