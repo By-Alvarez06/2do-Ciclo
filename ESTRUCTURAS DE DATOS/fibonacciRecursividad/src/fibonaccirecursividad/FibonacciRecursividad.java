809da97841f26bd02ef303913083a8ca19ad905e
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-04-23 16:10:51 -0500

    Recursividad en clase

--- ESTRUCTURAS DE DATOS/fibonacciRecursividad/src/fibonaccirecursividad/FibonacciRecursividad.java
+++ ESTRUCTURAS DE DATOS/fibonacciRecursividad/src/fibonaccirecursividad/FibonacciRecursividad.java
@@ -7,6 +7,7 @@ public class FibonacciRecursividad {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
+        //Ciclo do while para casos de numeros <= 0
         do {
             System.out.print("Ingrese un valor para imprimir los n valores de la serie fibonacci: ");
             n = sc.nextInt();
@@ -14,7 +15,7 @@ public class FibonacciRecursividad {
                 System.out.println("No se puede mostrar numeros negativos de la serie\n");
             }
         } while(n <= 0);  
-        
+        //Ciclo for para imprimir los valores de fibonacci desde 0 a N
         for (int i = 0; i < n; i++){
             System.out.print(fibonacci(i) + " ");
         }
