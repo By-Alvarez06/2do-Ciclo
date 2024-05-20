6a7501a65be6227f2bf95ff53b9d1ba72432543b
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-20 15:40:34 -0500

    Clases S7

---
+++ ESTRUCTURAS DE DATOS/estudianteRegistro/src/estudianteregistro/Estudiante.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estudianteregistro;
+public class Estudiante {
+    private String cedula;
+    private String nombre;
+    private String direccion;
+    private String sexo;
+    private int edad;
+    private int telefono;
+    
+    public Estudiante(){
+        cedula = "";
+        nombre = "";
+        direccion = "";
+        sexo = "";
+        edad = 0;
+        telefono = 0;
+    }
+    
+    public void setCedula(String s){
+        cedula = s;
+    }
+    public void setNombre(String s){
+        nombre = s;
+    }
+    public void setDireccion(String s){
+        direccion = s;
+    }
+    public void setSexo(String s){
+        sexo = s;
+    }
+    public void setEdad(int n){
+        edad = n;
+    }
+    public void setTelefono(int n){
+        telefono = n;
+    }
+}
---
+++ ESTRUCTURAS DE DATOS/estudianteRegistro/src/estudianteregistro/estudianteRegistro.java
@@ -0,0 +1,11 @@
+package estudianteregistro;
+public class estudianteRegistro {
+    public static void main(String[] args) {
+        Estudiante[] arrayRegistro = new Estudiante[6];
+        for (int i = 0; i < arrayRegistro.length; i++) {
+            
+            
+        }
+    }
+    
+}
