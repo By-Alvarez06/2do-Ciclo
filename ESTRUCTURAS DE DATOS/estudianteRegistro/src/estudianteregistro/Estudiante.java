c5e940b01d83ac4780b903145f041c4d5815bb6d
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-05-20 16:03:17 -0500

    Clases S7

--- ESTRUCTURAS DE DATOS/estudianteRegistro/src/estudianteregistro/Estudiante.java
+++ ESTRUCTURAS DE DATOS/estudianteRegistro/src/estudianteregistro/Estudiante.java
@@ -3,15 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package estudianteregistro;
+
 public class Estudiante {
+
     private String cedula;
     private String nombre;
     private String direccion;
     private String sexo;
     private int edad;
     private int telefono;
-    
-    public Estudiante(){
+
+    public Estudiante() {
         cedula = "";
         nombre = "";
         direccion = "";
@@ -19,23 +21,52 @@ public class Estudiante {
         edad = 0;
         telefono = 0;
     }
-    
-    public void setCedula(String s){
+
+    public void setCedula(String s) {
         cedula = s;
     }
-    public void setNombre(String s){
+
+    public void setNombre(String s) {
         nombre = s;
     }
-    public void setDireccion(String s){
+
+    public void setDireccion(String s) {
         direccion = s;
     }
-    public void setSexo(String s){
+
+    public void setSexo(String s) {
         sexo = s;
     }
-    public void setEdad(int n){
+
+    public void setEdad(int n) {
         edad = n;
     }
-    public void setTelefono(int n){
+
+    public void setTelefono(int n) {
         telefono = n;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
     }
 }
