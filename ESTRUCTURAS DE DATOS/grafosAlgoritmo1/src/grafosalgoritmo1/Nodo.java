88c15434fef7cb5d54354af6e32de81ad12808d2
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-07-01 16:53:33 -0500

    Grafos Algoritmo 1

--- ESTRUCTURAS DE DATOS/grafosAlgoritmo1/src/grafosalgoritmo1/Nodo.java
+++ ESTRUCTURAS DE DATOS/grafosAlgoritmo1/src/grafosalgoritmo1/Nodo.java
@@ -3,6 +3,14 @@ package grafosalgoritmo1;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+Cuando una clase implementa la interfaz Comparable, se compromete a proporcionar 
+una implementación del método compareTo. Este método define un orden natural para 
+los objetos de esa clase. En este caso, implements Comparable<Nodo> indica que la 
+clase Nodo debe implementar el método compareTo para que los nodos puedan compararse 
+entre sí.
+*/
+
 public class Nodo implements Comparable<Nodo> {
     public final String nombre;  // Nombre del nodo
     public List<Arista> adyacencias;  // Lista de aristas adyacentes
@@ -20,6 +28,11 @@ public class Nodo implements Comparable<Nodo> {
         return nombre;
     }
 
+    /*
+    El método compareTo recibe un objeto de la clase Nodo (denominado otro) y 
+    compara la distancia mínima (distanciaMinima) del nodo actual (this) con la 
+    distancia mínima del nodo otro.
+    */
     @Override
     public int compareTo(Nodo otro) {
         return Double.compare(distanciaMinima, otro.distanciaMinima);
